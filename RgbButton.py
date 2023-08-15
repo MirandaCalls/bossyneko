895d75995c6145a1fe9a1bdf33cdf06d152aa411
alena <gmaidenmueller@gmail.com>
2023-08-15 09:17:43 -0500

    debounce button; replace main loop with interrupts

--- RgbButton.py
+++ RgbButton.py
@@ -1,12 +1,37 @@
-import utime
+from machine import Pin
 import random
+import time
 
 class RgbButton:
-    def __init__(self, ledPinRed, ledPinGreen, ledPinBlue, switchPin):
-        self.ledPinRed = ledPinRed
-        self.ledPinGreen = ledPinGreen
-        self.ledPinBlue = ledPinBlue
-        self.switchPin = switchPin
+    def __init__(self, ledNumRed, ledNumGreen, ledNumBlue, switchNum):
+        self.ledPinRed = Pin(ledNumRed, Pin.OUT)
+        self.ledPinGreen = Pin(ledNumGreen, Pin.OUT)
+        self.ledPinBlue = Pin(ledNumBlue, Pin.OUT)
+        self.switchPin = Pin(switchNum, Pin.IN, Pin.PULL_UP)
+        self.switchPressCallback = None
+        self.switchReleaseCallback = None
+        self.switchPin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.switchHandler)
+        self.lastSwitchValue = None
+
+    def switchHandler(self, _):
+        time.sleep_ms(80)
+
+        currentValue = self.switchPin.value()
+        if currentValue == self.lastSwitchValue:
+            return
+
+        if self.switchPin.value() == 1 and self.switchReleaseCallback != None:
+            self.switchReleaseCallback()
+        elif self.switchPressCallback != None:
+            self.switchPressCallback()
+
+        self.lastSwitchValue = currentValue
+
+    def setSwitchPressHandler(self, callback):
+        self.switchPressCallback = callback
+
+    def setSwitchReleaseHandler(self, callback):
+        self.switchReleaseCallback = callback
 
     def assignRandomColor(self):
         self.ledPinRed.value(random.randint(0,1))
--- makefile
+++ makefile
@@ -1,3 +1,7 @@
 install:
 	ampy --port /dev/ttyACM0 put main.py
 	ampy --port /dev/ttyACM0 put RgbButton.py
+
+run:
+	make install
+	ampy --port /dev/ttyACM0 run -n main.py
